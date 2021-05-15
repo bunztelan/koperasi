bebbd34f77c5a90e52215f5bac65ef487fbe03a1
Nashir <nashiruddin.abubakar@gmail.com>
2021-05-15 19:22:08 +0700

    [ADD] navigation

---
+++ lib/config/route/route_name.dart
@@ -0,0 +1,15 @@
+/// Route name directory
+class RouteName {
+  // General routes
+  static const String generalSplash = '/';
+  static const String generalSignUpSuccess = '/signup_success';
+  static const String generalError = '/error';
+  static const String generalOrderSuccess = '/order_success';
+  static const String generalConfirmationMail = '/confirmation_mail';
+
+  static const String authSignIn = '/signin';
+  static const String authSignUp = '/signup';
+  static const String userDashboard = '/dashboard';
+  static const String userOrderCheckout = '/checkout';
+  static const String userManageAddress = '/manage_address';
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
+import 'package:beamer/beamer.dart';
 import 'package:flutter/material.dart';
-import 'package:k2ms_v2/ui/pages/single/confirmation_mail_page.dart';
 import 'package:k2ms_v2/ui/pages/pages.dart';
-import 'package:k2ms_v2/ui/pages/single/empty_order_page.dart';
-import 'package:k2ms_v2/ui/pages/single/order_made_page.dart';
+import 'package:k2ms_v2/ui/pages/single/confirmation_mail_page.dart';
 import 'package:k2ms_v2/ui/pages/single/signup_complete_page.dart';
 
+import 'config/route/route_name.dart';
 import 'config/theme_config.dart';
 
 void main() {
@@ -12,13 +12,28 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  final _routerDelegate = BeamerRouterDelegate(
+    locationBuilder: SimpleLocationBuilder(
+      routes: {
+        '/': (context) => SignInPage(),
+        RouteName.authSignIn: (context) => SignInPage(),
+        RouteName.authSignUp: (context) => SignUpPage(),
+        RouteName.userManageAddress: (context) => AddAddressPage(),
+        RouteName.generalConfirmationMail: (context) => ConfirmationMailPage(),
+        RouteName.generalSignUpSuccess: (context) => SignUpCompletePage(),
+        RouteName.userDashboard: (context) => DashboardPage(),
+      },
+    ),
+  );
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       title: 'Flutter Demo',
       theme: defaultStyleConfig,
-      home:DashboardPage(),
+      routeInformationParser: BeamerRouteInformationParser(),
+      routerDelegate: _routerDelegate,
     );
   }
 }
