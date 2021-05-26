91b3653f24f254b593b2f894ea848f87161f7790
Virginia Hendras <hendzcode@gmail.com>
2021-05-26 12:47:07 +0700

    Clean code and add behavior on login screen

--- lib/ui/pages/pages.dart
+++ lib/ui/pages/pages.dart
@@ -15,7 +15,7 @@ import 'package:k2ms_v2/ui/widgets/widgets.dart';
 import 'package:supercharged/supercharged.dart';
 
 part 'general_page.dart';
-part 'sign_in_page.dart';
+part 'auth/sign_in_page.dart';
 part 'sign_up_page.dart';
 part 'add_address_page.dart';
 part 'dashboard_page.dart';
