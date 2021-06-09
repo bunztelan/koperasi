2f3427c04649655dec317133e6cd3a3f30dfde26
Virginia Hendras <hendzcode@gmail.com>
2021-06-09 22:42:43 +0700

    Get current location when pick map address

--- lib/services/services.dart
+++ lib/services/services.dart
@@ -1,12 +1,10 @@
-import 'dart:convert';
-
 import 'package:dio/dio.dart';
 import 'package:k2ms_v2/models/plant.dart';
 import 'package:k2ms_v2/models/response/api_return_value.dart';
 import 'package:k2ms_v2/models/user.dart';
 
-part 'user_services.dart';
 part 'plant_services.dart';
+part 'user_services.dart';
 
 const platform = 'customer';
 const somethingWentWrongMsg = 'Terjadi kesalahan, coba lagi.';
--- lib/ui/widgets/map_picker.dart
+++ lib/ui/widgets/map_picker.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'dart:async';
+import 'package:geolocator/geolocator.dart';
 
 import 'package:k2ms_v2/config/color_config.dart';
 
@@ -15,61 +16,126 @@ class MapPicker extends StatefulWidget {
 
 class _MapPickerState extends State<MapPicker> {
   Completer<GoogleMapController> _controller = Completer();
-
   String post = '';
-
-  static final CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
+  Position _current;
+  CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(0, 0),
     zoom: 14.4746,
   );
 
+  Future<bool> _initData() async {
+    _current = await _determinePosition();
+
+    setState(() {
+      _kGooglePlex = CameraPosition(
+        target: LatLng(
+          _current.latitude.toDouble(),
+          _current.longitude.toDouble(),
+        ),
+        zoom: 14.4746,
+      );
+    });
+
+    widget.onLatLongChange(
+      _current.latitude.toString(),
+      _current.longitude.toString(),
+    );
+
+    return true;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _initData();
+  }
+
+  /// Set current position
+  Future<Position> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    return await Geolocator.getCurrentPosition();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Stack(
-          children: [
-            GoogleMap(
-              myLocationEnabled: true,
-              mapType: MapType.normal,
-              initialCameraPosition: _kGooglePlex,
-              onMapCreated: (GoogleMapController controller) {
-                _controller.complete(controller);
-              },
-              onCameraMove: (position) {
-                widget.onLatLongChange(position.target.latitude.toString(),
-                    position.target.longitude.toString());
-              },
-            ),
-            Align(
-              alignment: Alignment.center,
-              child: Icon(
-                Icons.location_pin,
-                size: 48,
-                color: AppColor.roseColor,
+    return FutureBuilder(
+      future: _initData(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return Scaffold(
+            body: SafeArea(
+              child: Stack(
+                children: [
+                  GoogleMap(
+                    myLocationEnabled: true,
+                    mapType: MapType.normal,
+                    initialCameraPosition: _kGooglePlex,
+                    onMapCreated: (GoogleMapController controller) {
+                      _controller.complete(controller);
+                    },
+                    onCameraMove: (position) {
+                      widget.onLatLongChange(
+                          position.target.latitude.toString(),
+                          position.target.longitude.toString());
+                    },
+                  ),
+                  Align(
+                    alignment: Alignment.center,
+                    child: Icon(
+                      Icons.location_pin,
+                      size: 48,
+                      color: AppColor.roseColor,
+                    ),
+                  ),
+                ],
               ),
             ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: () {
-          Navigator.pop(context);
-        },
-        isExtended: true,
-        icon: Icon(
-          Icons.location_pin,
-          color: AppColor.textPrimaryColor,
-        ),
-        label: Text(
-          'Pin lokasi',
-          style: Theme.of(context)
-              .textTheme
-              .bodyText2
-              .copyWith(color: AppColor.textPrimaryColor),
-        ),
-        backgroundColor: AppColor.primaryColor,
-      ),
+            floatingActionButton: FloatingActionButton.extended(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              isExtended: true,
+              icon: Icon(
+                Icons.location_pin,
+                color: AppColor.textPrimaryColor,
+              ),
+              label: Text(
+                'Pin lokasi',
+                style: Theme.of(context)
+                    .textTheme
+                    .bodyText2
+                    .copyWith(color: AppColor.textPrimaryColor),
+              ),
+              backgroundColor: AppColor.primaryColor,
+            ),
+          );
+        }
+        return Container();
+      },
     );
   }
 }
