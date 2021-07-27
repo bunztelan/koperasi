2cd3c6c8089b358e63596e494bea16f66c95a174
hendzee <hendzcode@gmail.com>
2021-07-27 11:59:12 +0700

    :art: Update edit profile feature

--- lib/ui/pages/profile/update_profile_page.dart
+++ lib/ui/pages/profile/update_profile_page.dart
@@ -19,7 +19,7 @@ class _UpdateProfilePageState extends State<UpdateProfilePage> {
 
   TextEditingController _nameController = TextEditingController();
   TextEditingController _emailController = TextEditingController();
-  TextEditingController _nipController = TextEditingController();
+  String _nipText = '';
   TextEditingController _plantController = TextEditingController();
   List<Plant> _plants = [];
   String _selectedPlant = '0';
@@ -30,7 +30,7 @@ class _UpdateProfilePageState extends State<UpdateProfilePage> {
     _nameController.text = BlocProvider.of<UserCubit>(context).state.user.name;
     _emailController.text =
         BlocProvider.of<UserCubit>(context).state.user.email;
-    _nipController.text = BlocProvider.of<UserCubit>(context).state.user.nip;
+    _nipText = BlocProvider.of<UserCubit>(context).state.user.nip;
 
     super.initState();
   }
@@ -75,64 +75,10 @@ class _UpdateProfilePageState extends State<UpdateProfilePage> {
         email: _emailController.text.toString(),
         maritalStatus:
             _marital == UpdateMaritalStatus.MARIED ? 'MARIED' : 'SINGLE',
-        nip: _nipController.text.toString(),
+        nip: _nipText,
         plantId: _selectedPlant,
       );
     }
-  }
-
-  /// Show list of plants dialog
-  void _showPlantsDialog() {
-    showDialog(
-      context: context,
-      barrierDismissible: true,
-      builder: (context) {
-        return Dialog(
-          child: Container(
-            width: MediaQuery.of(context).size.width - 30,
-            height: MediaQuery.of(context).size.height - 40,
-            child: SingleChildScrollView(
-              child: _plants.length < 1
-                  ? Container()
-                  : Column(
-                      children: _plants.map(
-                        (e) {
-                          var index = _plants.indexOf(e);
-
-                          return GestureDetector(
-                            onTap: () {
-                              setState(() {
-                                _selectedPlant = e.id.toString();
-                              });
-                              _plantController.text = e.name;
-                              Navigator.pop(context);
-                            },
-                            child: Container(
-                              width: double.infinity,
-                              margin: EdgeInsets.only(
-                                top: index == 0 ? 16 : 0,
-                                bottom: 16,
-                                right: 16,
-                                left: 16,
-                              ),
-                              padding: EdgeInsets.only(bottom: 16),
-                              decoration: BoxDecoration(
-                                  border: Border(
-                                bottom: BorderSide(
-                                  color: AppColor.black70,
-                                ),
-                              )),
-                              child: Text(e.name),
-                            ),
-                          );
-                        },
-                      ).toList(),
-                    ),
-            ),
-          ),
-        );
-      },
-    );
   }
 
   @override
@@ -150,7 +96,7 @@ class _UpdateProfilePageState extends State<UpdateProfilePage> {
               email: _emailController.text,
               maritalStatus:
                   _marital == UpdateMaritalStatus.MARIED ? 'maried' : 'single',
-              nip: _nipController.text,
+              nip: _nipText,
               plantId: _selectedPlant,
               oldUser: context.read<UserCubit>().state.user,
             );
@@ -213,52 +159,6 @@ class _UpdateProfilePageState extends State<UpdateProfilePage> {
                             ),
                           ),
                           SizedBox(height: 16),
-                          Text("Status Pernikahan",
-                              style: Theme.of(context).textTheme.bodyText1),
-                          SizedBox(height: 6),
-                          Container(
-                            padding: EdgeInsets.symmetric(vertical: 7),
-                            decoration: BoxDecoration(
-                                color: AppColor.black70,
-                                borderRadius: BorderRadius.circular(10)),
-                            child: Row(
-                              mainAxisSize: MainAxisSize.max,
-                              children: [
-                                Row(
-                                  children: [
-                                    Radio<UpdateMaritalStatus>(
-                                      activeColor: AppColor.textPrimaryColor,
-                                      value: UpdateMaritalStatus.SINGLE,
-                                      groupValue: _marital,
-                                      onChanged: (UpdateMaritalStatus value) {
-                                        setState(() {
-                                          _marital = value;
-                                        });
-                                      },
-                                    ),
-                                    Text('Lajang'),
-                                  ],
-                                ),
-                                SizedBox(width: 24),
-                                Row(
-                                  children: [
-                                    Radio<UpdateMaritalStatus>(
-                                      value: UpdateMaritalStatus.MARIED,
-                                      activeColor: AppColor.textPrimaryColor,
-                                      groupValue: _marital,
-                                      onChanged: (UpdateMaritalStatus value) {
-                                        setState(() {
-                                          _marital = value;
-                                        });
-                                      },
-                                    ),
-                                    Text('Menikah'),
-                                  ],
-                                ),
-                              ],
-                            ),
-                          ),
-                          SizedBox(height: 16),
                           Text("Alamat Email",
                               style: Theme.of(context).textTheme.bodyText1),
                           SizedBox(height: 6),
@@ -290,42 +190,6 @@ class _UpdateProfilePageState extends State<UpdateProfilePage> {
                               hintText: "bambang@gmail.com",
                             ),
                           ),
-                          SizedBox(height: 16),
-                          Text("NIP",
-                              style: Theme.of(context).textTheme.bodyText1),
-                          SizedBox(height: 6),
-                          TextFormField(
-                            controller: _nipController,
-                            validator: (val) {
-                              if (val.trim().isEmpty) {
-                                return 'NIP tidak boleh kosong';
-                              }
-                              return null;
-                            },
-                            style: GoogleFonts.poppins(fontSize: 14),
-                            decoration: InputDecoration(
-                              contentPadding: EdgeInsets.symmetric(
-                                vertical: 18,
-                                horizontal: 12,
-                              ),
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10),
-                                borderSide: BorderSide(
-                                  color: AppColor.primaryColor,
-                                  width: 1,
-                                ),
-                              ),
-                              hintStyle: GoogleFonts.poppins(
-                                color: AppColor.softGrayColor,
-                                fontSize: 14,
-                              ),
-                              hintText: "199802122015051003",
-                            ),
-                          ),
-                          SizedBox(height: 16),
-                          Text("Plant",
-                              style: Theme.of(context).textTheme.bodyText1),
-                          SizedBox(height: 6),
                           BlocProvider(
                             create: (context) => _plantCubit,
                             child: Builder(
@@ -353,59 +217,7 @@ class _UpdateProfilePageState extends State<UpdateProfilePage> {
                                               .status);
                                     }
                                   },
-                                  child: BlocBuilder<PlantCubit, PlantState>(
-                                    bloc: BlocProvider.of<PlantCubit>(context),
-                                    builder: (context, state) {
-                                      if (state is PlantLoadedState) {
-                                        return GestureDetector(
-                                          onTap: _showPlantsDialog,
-                                          child: TextFormField(
-                                            controller: _plantController,
-                                            enabled: false,
-                                            enableInteractiveSelection: false,
-                                            validator: (val) {
-                                              if (val.trim().isEmpty) {
-                                                return 'Plant tidak boleh kosong';
-                                              }
-                                              return null;
-                                            },
-                                            style: GoogleFonts.poppins(
-                                                fontSize: 14),
-                                            decoration: InputDecoration(
-                                              contentPadding:
-                                                  EdgeInsets.symmetric(
-                                                vertical: 18,
-                                                horizontal: 12,
-                                              ),
-                                              border: OutlineInputBorder(
-                                                borderRadius:
-                                                    BorderRadius.circular(10),
-                                                borderSide: BorderSide(
-                                                  color: AppColor.primaryColor,
-                                                  width: 1,
-                                                ),
-                                              ),
-                                              disabledBorder:
-                                                  OutlineInputBorder(
-                                                borderRadius:
-                                                    BorderRadius.circular(10),
-                                                borderSide: BorderSide(
-                                                  color: AppColor.borderColor,
-                                                  width: 1,
-                                                ),
-                                              ),
-                                              hintStyle: GoogleFonts.poppins(
-                                                color: AppColor.softGrayColor,
-                                                fontSize: 14,
-                                              ),
-                                              hintText: "Plant A",
-                                            ),
-                                          ),
-                                        );
-                                      }
-                                      return Container();
-                                    },
-                                  ),
+                                  child: Container(),
                                 );
                               },
                             ),
